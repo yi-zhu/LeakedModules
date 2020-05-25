83488b32991be627ee8dc8cc39a1910c03d39274
astealz <astealz@gmail.com>
2020-05-25 15:26:38 +0500

    added dogtag patch

---
+++ EmuTarkov.SinglePlayer/Patches/Dogtag/DogtagPatch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using EFT;
+using EFT.InventoryLogic;
+using EmuTarkov.Common.Utils.Patching;
+using UnityEngine;
+using Equipment = GClass1526;
+using DamageInfo = GStruct204;
+
+namespace EmuTarkov.SinglePlayer.Patches.Dogtag
+{
+    class DogtagPatch : AbstractPatch
+    {
+        private static readonly Func<Player, Equipment> getEquipmentProperty;
+
+        static DogtagPatch()
+        {
+            getEquipmentProperty = typeof(Player)
+                .GetProperty("Equipment", BindingFlags.NonPublic | BindingFlags.Instance)
+                .GetGetMethod(true)
+                .CreateDelegate(typeof(Func<Player, Equipment>)) as Func<Player, Equipment>;
+        }
+
+        public override MethodInfo TargetMethod()
+        {
+            return typeof(Player)
+                .GetMethod("OnBeenKilledByAggressor", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        public static void Postfix(Player __instance, Player aggressor, DamageInfo damageInfo)
+        {
+            if (__instance.Profile.Info.Side == EPlayerSide.Savage)
+                return;
+
+            var equipment = getEquipmentProperty(__instance);
+            var dogtagSlot = equipment.GetSlot(EFT.InventoryLogic.EquipmentSlot.Dogtag);
+
+            var dogtagItem = dogtagSlot.ContainedItem as Item;
+            if (dogtagItem == null)
+            {
+                Debug.LogError("DogtagPatch error > DogTag slot item is null somehow.");
+                return;
+            }
+
+            DogtagComponent itemComponent = dogtagItem.GetItemComponent<DogtagComponent>();
+
+            if (itemComponent == null)
+            {
+                Debug.LogError("DogtagPatch error > DogTagComponent on dog tag slot is null. Something went horrifically wrong!");
+                return;
+            }
+
+            itemComponent.Nickname = __instance.Profile.Info.Nickname;
+            itemComponent.Side = __instance.Profile.Info.Side;
+            itemComponent.KillerName = aggressor.Profile.Info.Nickname;
+            itemComponent.Time = DateTime.Now;
+            itemComponent.Status = "Killed by";
+            itemComponent.WeaponName = damageInfo.Weapon.Name;
+
+            if (__instance.Profile.Info.Experience > 0)
+                itemComponent.Level = __instance.Profile.Info.Level;
+        }
+    }
+}
